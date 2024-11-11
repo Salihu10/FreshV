413286c7566a45153307b7bd5c8bfb039f5b8bd0
sali <salihubasiru@outlook.at>
2024-11-11 10:27:07 +0100

    Initial commit of fresh code

---
+++ freshV/src/main/java/com/example/freshV/FreshVApplication.java
@@ -0,0 +1,13 @@
+package com.example.freshV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVApplication.class, args);
+	}
+
+}
---
+++ freshV/src/test/java/com/example/freshV/FreshVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freshV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
